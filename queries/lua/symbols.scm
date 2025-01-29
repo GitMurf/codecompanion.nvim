110694872b11669ea4df9c6a52542fb9762c4edc
GitMurf <64155612+GitMurf@users.noreply.github.com>
2025-01-28 18:09:38 -0800

    feat(symbols): lua improvements; ts func params, return types, interfaces

--- queries/lua/symbols.scm
+++ queries/lua/symbols.scm
@@ -1,17 +1,42 @@
 ; Ref: https://github.com/stevearc/aerial.nvim/blob/master/queries/lua/aerial.scm
 ; MIT License
 
-(variable_declaration
-  (assignment_statement
-    (expression_list
-      value: (_) @name))
+; Require statements at root, inside functions, tables etc.
+(_(_
+  name: (identifier) @name
   (#contains? @name "require")
-  (#set! "kind" "Import")) @symbol
+  (#set! "language" "lua")
+  (#set! "kind" "Import")
+)) @symbol
 
+; local function foo(bar)
 (function_declaration
-  name: [
-    (identifier)
-    (dot_index_expression)
-    (method_index_expression)
-  ] @name
-  (#set! "kind" "Function")) @symbol
+  name: (_) @name
+  parameters: (_)? @parameters
+  (#set! "language" "lua")
+  (#set! "kind" "Function")
+) @symbol
+
+; local foo = function(bar)
+; M.foo = function(bar)
+(assignment_statement
+  (variable_list
+    name: (_) @name) @var_list
+  (expression_list
+    value: (function_definition
+      parameters: (_)? @parameters
+    ) @func_def
+  ) @exp_list
+  (#set! "language" "lua")
+  (#set! "kind" "Function")
+) @symbol
+
+; Table => func: { foo = function(bar) end }
+(field
+  name: (identifier) @name
+  value: (function_definition
+    parameters: (_)? @parameters
+  ) @func_def
+  (#set! "language" "lua")
+  (#set! "kind" "Function")
+) @symbol

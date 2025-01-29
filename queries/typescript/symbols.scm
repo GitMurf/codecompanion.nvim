b1458fe707df9d3f700167f30b05237021b7e410
GitMurf <64155612+GitMurf@users.noreply.github.com>
2025-01-28 18:09:38 -0800

    feat(symbols): lua improvements; ts func params, return types, interfaces

--- queries/typescript/symbols.scm
+++ queries/typescript/symbols.scm
@@ -1,34 +1,114 @@
 ; Ref: https://github.com/stevearc/aerial.nvim/blob/master/queries/typescript/aerial.scm
 ; MIT License
 
-((import_statement) @name
- (#set! "kind" "Import")) @symbol
+; Imports
+(
+  (import_statement) @name
+  (#set! "language" "typescript")
+  (#set! "kind" "Import")
+) @symbol
 
-(function_signature
-  name: (identifier) @name
-  (#set! "kind" "Function")) @symbol
+; Regular functions
+(
+  "export"? @export
+  "default"? @default
+  (_
+    "async"? @async
+    "function" @symbol_keyword
+    "*"? @generator
+    name: (identifier) @name
+    (#has-parent? @name function_declaration generator_function_declaration)
+    type_parameters: (_)? @type_parameters
+    parameters: (_)? @parameters
+    return_type: (_)? @return_type
+  ) @symbol
+  (#has-parent? @symbol export_statement program statement_block)
+  (#set! "language" "typescript")
+  (#set! "kind" "Function")
+)
 
-(function_declaration
-  name: (identifier) @name
-  (#set! "kind" "Function")) @symbol
+; Arrow functions
+(
+  "export"? @export
+  (_
+    ["var" "let" "const"] @symbol_keyword
+    (variable_declarator
+      name: (identifier) @name
+      value: (arrow_function
+        "async"? @async
+        type_parameters: (_)? @type_parameters
+        parameters: (_)? @parameters
+        return_type: (_)? @return_type
+      ) @arrow
+    ) @var_declare
+    (#has-parent? @var_declare lexical_declaration variable_declaration)
+  ) @symbol
+  (#has-parent? @symbol export_statement program statement_block)
+  (#set! "language" "typescript")
+  (#set! "kind" "Function")
+)
 
-(generator_function_declaration
-  name: (identifier) @name
-  (#set! "kind" "Function")) @symbol
+; Classes
+(
+  "export"? @export
+  "default"? @default
+  (_
+    "abstract"? @abstract_class
+    "class" @symbol_keyword
+    name: (type_identifier) @name
+    (class_heritage
+      (extends_clause
+        value: (identifier)?
+      )? @inherit
+    )?
+    (class_heritage
+      (implements_clause
+        (type_identifier)?
+      )? @inherit
+    )?
+    (#has-parent? @name class_declaration abstract_class_declaration)
+  ) @symbol
+  (#has-parent? @symbol export_statement program)
+  (#set! "language" "typescript")
+  (#set! "kind" "Class")
+)
 
-(class_declaration
-  name: (type_identifier) @name
-  (#set! "kind" "Class")) @symbol
-
-(interface_declaration
-  name: (type_identifier) @name
-  (#set! "kind" "Interface")) @symbol
-
-(method_definition
+; Class Methods
+(_
+  (accessibility_modifier)? @acc_modifier
+  "abstract"? @abstract_method
+  "async"? @async
+  "*"? @generator
   name: (property_identifier) @name
-  (#set! "kind" "Method")) @symbol
+  (#has-parent? @name method_definition abstract_method_signature)
+  type_parameters: (_)? @type_parameters
+  parameters: (_)? @parameters
+  return_type: (_)? @return_type
+  (#set! "language" "typescript")
+  (#set! "kind" "Method")
+) @symbol
 
+; Arrow Functions in Class Properties
 (public_field_definition
+  (accessibility_modifier)? @acc_modifier
+  "static"? @static
+  "readonly"? @read_only
   name: (property_identifier) @name
-  value: (arrow_function)
-  (#set! "kind" "Method")) @symbol
+  value: (arrow_function
+    "async"? @async
+    type_parameters: (_)? @type_parameters
+    parameters: (_)? @parameters
+    return_type: (_)? @return_type
+  ) @arrow
+  (#set! "language" "typescript")
+  (#set! "kind" "Method")
+) @symbol
+
+; Interfaces and Type Aliases
+(_
+  ["interface" "type"] @symbol_keyword
+  name: (type_identifier) @name
+  (#has-parent? @name type_alias_declaration interface_declaration)
+  (#set! "language" "typescript")
+  (#set! "kind" "Interface")
+) @symbol
